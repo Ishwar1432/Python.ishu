0913ee2b48eb2fa23e95ecc5ac75fffd659e140e
Ishwar1432 <141400158+Ishwar1432@users.noreply.github.com>
2024-01-28 00:26:51 +0530

    Create He'll.py

---
+++ He'll.py
@@ -0,0 +1,9 @@
+# python program to print hello world
+print("hello world")
+echo "# Hello-" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Ishwar1432/Hello-.git
+git push -u origin main
